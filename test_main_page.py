0e49e155c6ec6ef09a4c3df517be74c8cb818428
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2021-06-03 02:36:57 +0300

    remove skipping test

--- test_main_page.py
+++ test_main_page.py
@@ -11,7 +11,7 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-@pytest.mark.skip
+
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
