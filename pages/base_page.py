f0bacb55d5739b1bd91d2c9be62f079bb8b190f0
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2021-06-03 02:37:18 +0300

    add nosuch el exception

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,7 @@
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import math
 
 class BasePage:
     def __init__(self, browser: str, url: str, timeout=10):
@@ -21,5 +24,20 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
+    
+    def solve_quiz_and_get_code(self):
+        WebDriverWait(self.browser, 3).until(EC.alert_is_present())
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except (NoAlertPresentException, TimeoutException):
+            print("No second alert presented")
 
         
