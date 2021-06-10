eed7a6bc873765e8dbf1f6b26aef5d1de8ad1b94
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2021-06-11 00:35:35 +0300

    add checking the success message

--- pages/product_page.py
+++ pages/product_page.py
@@ -33,4 +33,11 @@ class ProductPage(BasePage):
         # get the total price
         total_price = self.browser.find_element(*ProductPageLocators.TOTAL_PRICE).text
         assert product_price in total_price, "The price is not equal"
- 
+    
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_be_dissapeared_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Message is presented, but should not be"
