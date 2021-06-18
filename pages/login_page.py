aaca119c237c026e2fc78d479345d9da70e2402f
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2021-06-18 21:37:36 +0300

    add assert for authorized user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from pages.base_page import BasePage
-from pages.locators import LoginPageLocators
+from pages.locators import LoginPageLocators, BasePageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -15,3 +15,7 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "There's no registration form"
+    
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                    " probably unauthorised user"
