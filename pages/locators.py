f2c9c3781600ceef3322bbf44266eb3668154198
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2021-06-18 21:37:18 +0300

    add registration locators

--- pages/locators.py
+++ pages/locators.py
@@ -6,6 +6,10 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL = (By.NAME, "registration-email")
+    PASSWORD = (By.NAME, "registration-password1")
+    CONFIRM_PASSWORD = (By.NAME, "registration-password2")
+    SUBMIT_BUTTON = (By.CSS_SELECTOR, "button.btn-primary[value='Register']")
 
 class ProductPageLocators:
     ADD_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
@@ -17,3 +21,9 @@ class ProductPageLocators:
 
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+class BasketPageLocators:
+    BASKET_ITEMS = (By.CLASS_NAME, "basket-items")
+    TEXT_EMPTY_BASKET = (By.ID, "content_inner")
+    BASKET_BUTTON = (By.CSS_SELECTOR, "span.btn-group")
